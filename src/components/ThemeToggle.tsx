db3651968870fad977ffc32261396a0f4e6a9365
fMuchka <f.muchka@gmail.com>
2025-12-13 21:36:48 +0100

    F ThemeToggle component

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { ThemeMode, toggleThemeMode } from '../utils/themeSettings';
+import { Brightness7, Brightness4 } from '@mui/icons-material';
+import { Button } from '@mui/material';
+
+export interface ThemeToggleProps {
+  /**
+   * The current theme mode (e.g., 'light', 'dark').
+   */
+  mode: ThemeMode;
+
+  /**
+   *  Callback function invoked when the theme toggle is triggered.
+   *    The function receives the new theme mode as a parameter.
+   */
+  onToggle: (mode: ThemeMode) => void;
+}
+
+/**
+ * A button component that toggles between light and dark theme modes.
+ *
+ * @component
+ * @param props - The component props {@link ThemeToggleProps}
+ *
+ * @returns  A button that displays a sun icon in light mode or moon icon in dark mode
+ *
+ * @example
+ * ```tsx
+ * <ThemeToggle
+ *   mode="dark"
+ *   onToggle={(newTheme) => console.log('Theme changed to:', newTheme)}
+ * />
+ * ```
+ */
+export const ThemeToggle = (props: ThemeToggleProps) => {
+  const { mode, onToggle } = props;
+
+  const [localMode, setLocalMode] = useState<ThemeMode>(mode);
+
+  const handleClick = () => {
+    const newTheme = toggleThemeMode();
+    setLocalMode(newTheme);
+
+    onToggle(newTheme);
+  };
+
+  return (
+    <Button
+      variant="outlined"
+      color="inherit"
+      onClick={() => handleClick()}
+      aria-label={localMode === 'dark' ? 'switch to light mode' : 'switch to dark mode'}
+    >
+      {localMode === 'dark' ? <Brightness4 /> : <Brightness7 />}
+    </Button>
+  );
+};
+
+export default ThemeToggle;
